5f1ee8cd922b637dcc27e0e23151de568a141e8e
ReeceJones <reece_jones@icloud.com>
2021-10-05 16:51:30 -0400

    Add half-functional data parser

---
+++ lib/parser.py
@@ -0,0 +1,17 @@
+import struct
+
+def read_vectors(f, parser='fvec'):
+    """
+    Read vector files asynchronously.
+    """
+    BLOCK_SIZE = 4096
+    WORD_SIZE = 1 if parser=='bvec' else 4
+    raw = f.read(BLOCK_SIZE)
+    while len(raw) > 0:
+        vec_size = struct.unpack('<i', raw[0:WORD_SIZE])[0]
+        while len(raw) > WORD_SIZE + WORD_SIZE * vec_size:
+            s = raw[WORD_SIZE:WORD_SIZE + WORD_SIZE*vec_size]
+            yield struct.unpack(f'<{vec_size}f', s)
+            raw = raw[WORD_SIZE + WORD_SIZE*vec_size:]
+        raw = raw + f.read(BLOCK_SIZE)
+
