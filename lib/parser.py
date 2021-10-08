538401a377a167f9869c46cdc2f3c3197ce2bb90
ReeceJones <reece_jones@icloud.com>
2021-10-08 10:56:10 -0400

    Make parser more functional

--- lib/parser.py
+++ lib/parser.py
@@ -6,12 +6,15 @@ def read_vectors(f, parser='fvec'):
     """
     BLOCK_SIZE = 4096
     WORD_SIZE = 1 if parser=='bvec' else 4
+    WORD_TYPE = parser[0]
     raw = f.read(BLOCK_SIZE)
     while len(raw) > 0:
         vec_size = struct.unpack('<i', raw[0:WORD_SIZE])[0]
         while len(raw) > WORD_SIZE + WORD_SIZE * vec_size:
             s = raw[WORD_SIZE:WORD_SIZE + WORD_SIZE*vec_size]
-            yield struct.unpack(f'<{vec_size}f', s)
+            yield struct.unpack(f'<{vec_size}{WORD_TYPE}', s)
             raw = raw[WORD_SIZE + WORD_SIZE*vec_size:]
-        raw = raw + f.read(BLOCK_SIZE)
-
+        raw_read = f.read(BLOCK_SIZE)
+        if len(raw_read) <= 0:
+            break
+        raw = raw + raw_read
