aa45ffbd89e8457db2a7ea6105346a09aba317f9
ReeceJones <reece_jones@icloud.com>
2021-10-29 09:42:36 -0400

    get things working

--- vamana.py
+++ vamana.py
@@ -32,6 +32,20 @@ def GreedySearch(s, x, k, L, G):
 
     return set(sorted(list(A), key=lambda y: numpy.linalg.norm(numpy.array(y) - numpy.array(x)))[:k]),B
 
+def LightRobustPrune(p, V, alpha, R, G):
+    V = V.union(G[tuple(p)]) - {tuple(p)}
+    G[tuple(p)] = set()
+    while len(V) > 0:
+        _V = list(V)
+        norms = [numpy.linalg.norm(numpy.array(y) - p) for y in _V]
+        p_prime = _V[numpy.argmin(norms)]
+        G[tuple(p)] = G[tuple(p)].union({p_prime})
+        if len(G[tuple(p)]) == R:
+            break
+        for i in range(len(_V)):
+            if alpha*numpy.linalg.norm(numpy.array(p_prime) - _V[i]) <= norms[i]:
+                V.remove(_V[i])
+    return G
 
 def medoid(df):
     distMatrix = pairwise_distances(df)
@@ -41,6 +55,7 @@ def VamanaAlgo(P:pd.DataFrame, a:float, L, R):
     G = {}
     s = medoid(P) 
     sigma = P.sample(frac=1)
+    lens = set()
     #Initializing G to an adjacency matrix where each point has R random out-neighbors MUST OPTIMIZE
     print('building adjacency matrix')
     for index in range(len(sigma)):
@@ -48,16 +63,18 @@ def VamanaAlgo(P:pd.DataFrame, a:float, L, R):
         tuppoint = tuple(sigma.iloc[index])
         npsig = sigma.drop(index=index, axis=0, inplace=False)
         G[tuppoint] = set(tuple(i) for i in npsig.sample(n=R, axis=0).to_numpy())
+        lens = lens.union({len(i) for i in G[tuppoint]})
+    print(f'Adjacency lens: {lens}')
     print('finished building adjacency matrix')
     for index in range(len(sigma)):
         point = tuple(sigma.iloc[index])
         L_, V = GreedySearch(sigma.iloc[s], point, 20, L, G) # must implement GreedySearch, this is placeholder
         print(index)
-        RobustPrune.robustPrune(point, V, a, R)
+        G = LightRobustPrune(point, V, a, R, G)
         for j in G[point]:
-            z = G[j].union(point)
+            z = G[j].union({point})
             if len(z) > R:
-                RobustPrune.robustPrune(j, z, a, R)
+                G = LightRobustPrune(j, z, a, R, G)
             else:
                 G[j] = z
 
