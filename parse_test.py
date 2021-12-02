feecfb1974afc79e8404e086dfe5e8e303f18a7f
Aidan Abbott <77586827+aabbott1216@users.noreply.github.com>
2021-12-02 14:26:10 -0500

    changed to return np array instead of pandas df

--- parse_test.py
+++ parse_test.py
@@ -1,9 +1,10 @@
 from lib import parser
 import pandas as pd
+import numpy as np
 
 def parse_to_df(filepath):
     l = []
     with open(filepath, "rb") as f:
         for val in parser.read_vectors(f):
             l.append(val)
-    return pd.DataFrame(l)
+    return np.array(l)
